ff3c20e03bb0343fced73ed11a7050980ac01925
abdurrahman086 <rahmanabdur15689@gmail.com>
2026-06-20 07:20:15 +0700

    perbaikan: tambahkan opsi nitro true di config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,14 @@
+// import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+
+// export default defineConfig({
+//   tanstackStart: {
+//     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
+//     // nitro/vite builds from this
+//     server: { entry: "server" },
+//   },
+// });
+
+
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
@@ -6,4 +17,6 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  // Tambahkan baris di bawah ini untuk memaksa plugin Nitro deploy aktif di Vercel
+  nitro: true,
 });
